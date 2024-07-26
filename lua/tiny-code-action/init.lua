9e052b5d0a9d2772a550c56d0aa9204fc85a129d
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-07-26 14:36:08 +0200

    fix: removed duplicated if

--- lua/tiny-code-action/init.lua
+++ lua/tiny-code-action/init.lua
@@ -103,11 +103,6 @@ local function code_action_finder(opts)
 		return nil
 	end
 
-	if err then
-		vim.notify("Error getting code actions: " .. vim.inspect(err), vim.log.levels.ERROR)
-		return {}
-	end
-
 	for client_id, buf_result in pairs(all_results) do
 		local client = vim.lsp.get_client_by_id(client_id)
 
