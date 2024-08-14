1805a4ac14f02b1c19f9f1e3aaeb18315c374f44
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-08-14 12:18:30 +0200

    fix: removed unused wrap_text. Fixes #16

--- lua/tiny-code-action/backend/vim.lua
+++ lua/tiny-code-action/backend/vim.lua
@@ -3,7 +3,6 @@ local M = {}
 
 local previewers = require("telescope.previewers")
 local lsp_actions = require("tiny-code-action.action")
-local utils = require("tiny-code-action.utils")
 
 function M.get_diff(old_lines, new_lines, opts)
 	return vim.diff(table.concat(old_lines, "\n"), table.concat(new_lines, "\n"), {
@@ -19,22 +18,20 @@ function M.create_previewer(opts, bufnr, backend, preview_action_callback)
 			local action = entry.value.action
 			local client = entry.value.client
 
+			if not action then
+				return
+			end
+
 			vim.api.nvim_set_option_value("filetype", "diff", { buf = self.state.bufnr })
 
 			if lsp_actions.action_is_not_complete(action) then
 				lsp_actions.resolve(action, bufnr, client, function(res, err)
 					local preview_lines = preview_action_callback(opts, res, backend, bufnr)
-					if not vim.tbl_isempty(preview_lines) then
-						preview_lines = utils.wrap_text(preview_lines, 80)
-					end
 
 					vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, preview_lines)
 				end)
 			else
 				local preview_lines = preview_action_callback(opts, action, backend, bufnr)
-				if not vim.tbl_isempty(preview_lines) then
-					preview_lines = utils.wrap_text(preview_lines, 80)
-				end
 
 				vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, preview_lines)
 			end
